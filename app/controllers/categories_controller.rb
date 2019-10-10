16a24a051fc86ec758858346ec366f7e733d7592
oliveira-andre <desmonplays@gmail.com>
2019-10-10 00:17:32 -0400

    (new feature) categories and restaurants controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all.order(:title)
+    render json: @categories
   end
 end
